94cfa72142cf5ab5d77605a46af1b23b63379bb9
bbeardsall <brianbeardsall@gmail.com>
2020-07-18 17:25:11 -0300

    Shiny app: added color, facet, x/y axis options

--- app.R
+++ app.R
@@ -1,50 +1,97 @@
 # Shiny app for GreenEdgeProtein
 library(tidyverse)
 
+test <- data %>%
+  filter(TotalChlaSheetName == "Nf_Growthvs.IceCamp")
+test
+
 data <- readRDS("ProcessData/GreenEdge_TargetProteinReport_.Rds") %>%
   filter(!is.na("Stress_h"))
 
 # User interface ----
 ui <- fluidPage(
-  titlePanel("censusVis"),
+  #titlePanel("GreenEdgeProtein"),
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Create demographic maps with 
-        information from the 2010 US Census."),
+      #helpText("Interactive plotting of GreenEdgeProtein quantification results."),
       
-      selectInput("normalizeMethod", 
-                  label = "Choose normalize",
-                  choices = c("fmol_ugTotalProtein", "fmol_mL"),
+      selectInput("xaxis", 
+                  label = "Choose x axis",
+                  choices = c("Growth_uE", "Stress_h"),
+                  selected = "Stress_h"),
+      
+      selectInput("yaxis", 
+                  label = "Choose y axis (normalize method)",
+                  choices = c("fmol_ugTotalProtein", "fmol_mL", "fmol_ugChla"),
                   selected = "fmol_ugTotalProtein"),
+      
       selectInput("target", 
                   label = "Choose target",
-                  choices = c("PsbA", "Fcp6"),
-                  selected = "PsbA")
-    ),
+                  choices = unique(data$Target),
+                  selected = "PsbA"),
+      
+      checkboxGroupInput("species", 
+                         h3("Choose species"), 
+                         choices = setNames(as.list(unique(data$Sp)), unique(data$Sp)),
+                         selected = unique(data$Sp)),
+      checkboxGroupInput("sheet", 
+                         h3("ChlA Sheet Name"), 
+                         choices = setNames(as.list(unique(data$TotalChlaSheetName)), unique(data$TotalChlaSheetName)),
+                         selected = unique(data$TotalChlaSheetName)),
+      
+      selectInput("color",
+                  label = "Choose color",
+                  choices = c("Sp", "Inhibitor", "None"),
+                  selected = "None"),
+      
+      selectInput("facet",
+                  label = "Choose facet",
+                  choices = c("Sp", "Inhibitor", "None"),
+                  selected = "None"),
+      ),
+    
+    
     
     mainPanel(plotOutput("speciesPlot"))
   )
 )
 
+
 # Server logic ----
 server <- function(input, output) {
   finalData <- reactive({
-    test <- data %>%
+    data %>%
       filter(Target == input$target,
-             Sp == "Chaetoceros neogracilis")
-    test
+             Sp %in% input$species,
+             TotalChlaSheetName %in% input$sheet)
   })
   
   output$speciesPlot <- renderPlot({
-    normalizeMethodSym <- sym(input$normalizeMethod)
+    yaxisSym <- sym(input$yaxis)
+    ColorSym <- sym(input$color)
+    FacetSym <- sym(input$facet)
+    xaxisSym <- sym(input$xaxis)
+    
     ggplot(finalData()) +
-      geom_point(aes(x = Stress_h, y = !!normalizeMethodSym))+#, colour = as.factor(CultureId))) +
-      facet_grid(vars(Sp), vars(Inhibitor))+
+      {if(input$color != "None"){
+        geom_point(aes(x = !!xaxisSym, y = !!yaxisSym, color = !!ColorSym))
+        }
+        else{
+          geom_point(aes(x = !!xaxisSym, y = !!yaxisSym))
+        }}+
+      #geom_point(aes(x = Stress_h, y = !!yaxisym, color = !!ColorSym))+#, colour = as.factor(CultureId))) +
+      {if(input$facet != "None"){
+        facet_wrap(vars(!!FacetSym))
+      }}+
+      #facet_wrap(vars(Inhibitor))+
+      #facet_grid(vars(Sp), vars(Inhibitor))+
       theme_bw()+
-      ggtitle("PsbA")
+      ggtitle(paste("Protein Target:", input$target))
   })
 }
+
+
 
 # Run app ----
 shinyApp(ui, server)
