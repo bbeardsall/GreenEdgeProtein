c0694178720b97aa19c4e4c943f8d87594c2642a
bbeardsall <brianbeardsall@gmail.com>
2020-07-20 11:12:20 -0300

    added Mireille's variable selection, slider for point size, cartesian faceting

--- ShinyGreenEdge/app.R
+++ ShinyGreenEdge/app.R
@@ -3,87 +3,134 @@ library(tidyverse)
 library(stats)
 library(shinydashboard)
 
+##### Change to shiny directory before deploying!! #####
+
+choiceVariables <- c("Sp", "Inhibitor", "CultureId", "BlotId", "None")
+
 data <- readRDS("data/GreenEdge_TargetProteinReport_.Rds") %>%
   filter(!is.na("Stress_h"))
 
 # User interface ----
 ui <- dashboardPage(
+  
   dashboardHeader(title = "GreenEdgeProtein"),
+  
   dashboardSidebar(
     width = 500,
     fluidRow(
-      box(background = "black",
-          selectInput("xaxis", 
-                      label = "X axis",
-                      choices = c("Growth_uE", "Stress_h"),
-                      selected = "Stress_h")
+      box(
+        background = "black",
+        selectInput(
+          "xaxis",
+          label = "X axis",
+          choices = c("Growth_uE", "Stress_h"),
+          selected = "Stress_h"
+        )
       ),
       box(
         background = "black",
-        selectInput("yaxis", 
-                    label = "Y axis",
-                    choices = c("fmol_ugTotalProtein", "fmol_mL", "fmol_ugChla"),
-                    selected = "fmol_ugTotalProtein")
+        selectInput(
+          "yaxis",
+          label = "Y axis",
+          choices = c("fmol_ugTotalProtein", "fmol_mL", "fmol_ugChla"),
+          selected = "fmol_ugTotalProtein"
+        )
       )
-      
     ),
     
     fluidRow(
       box(
-          background = "black",
-          selectInput("target", 
-                      label = "Target",
-                      choices = unique(data$Target),
-                      selected = "PsbA")
-          ),
-      
-      box(title = "Species",
-          background = "black",
-          checkboxGroupInput("species", 
-                             NULL, 
-                             choices = setNames(as.list(unique(data$Sp)), unique(data$Sp)),
-                             selected = unique(data$Sp))
-      )
-          
+        background = "black",
+        sliderInput(
+          "size",
+          label = "Size",
+          min = 0, max = 5, value = 0.5,
+          step = 0.5
+        )
       ),
+      box(
+        background = "black",
+        selectInput(
+          "target",
+          label = "Target",
+          choices = unique(data$Target),
+          selected = "PsbA"
+        )
+      ),
+      box(
+        background = "black",
+        selectInput(
+          "color",
+          label = "Choose color",
+          choices = choiceVariables,
+          selected = "None"
+        )
+      ),
+      box(
+        background = "black",
+        selectInput(
+          "facetWrap",
+          label = "Choose facet wrap",
+          choices = choiceVariables,
+          selected = "None"
+        )
+      ),
+      box(
+        background = "black",
+        selectInput(
+          "shape",
+          label = "Choose shape",
+          choices = choiceVariables,
+          selected = "None"
+        )
+      ),
+      box(
+        background = "black",
+        selectInput(
+          "facetX",
+          label = "Choose facet row",
+          choices = choiceVariables,
+          selected = "None"
+        )
+      ),
+      box(
+        background = "black",
+        selectInput(
+          "facetY",
+          label = "Choose facet column",
+          choices = choiceVariables,
+          selected = "None"
+        )
+      )
+    ),
+    
     fluidRow(
       box(
         background = "black",
-      
-      
-      checkboxGroupInput("sheet", 
-                         h3("ChlA Sheet Name"), 
-                         choices = setNames(as.list(unique(data$TotalChlaSheetName)), unique(data$TotalChlaSheetName)),
-                         selected = unique(data$TotalChlaSheetName))
+        checkboxGroupInput(
+          "sheet",
+          "ChlA Sheet Name",
+          choices = setNames(as.list(unique(
+            data$TotalChlaSheetName
+          )), unique(data$TotalChlaSheetName)),
+          selected = unique(data$TotalChlaSheetName)
+        )
       ),
       box(
         background = "black",
-        selectInput("color",
-                    label = "Choose color",
-                    choices = c("Sp", "Inhibitor", "None"),
-                    selected = "None")
-        ),
-      box(
-        background = "black",
-      selectInput("facet",
-                  label = "Choose facet",
-                  choices = c("Sp", "Inhibitor", "None"),
-                  selected = "None")
+        checkboxGroupInput(
+          "species",
+          "Species",
+          choices = setNames(as.list(unique(data$Sp)), unique(data$Sp)),
+          selected = unique(data$Sp)
+        )
       )
     )
-      
-        
-      ),
-      
-      
-      
-      
-    
-    dashboardBody(plotOutput("speciesPlot"),
-                  height = "5000px"
-                  )
+  ),
+  
+  dashboardBody(plotOutput("speciesPlot"),
+                height = "5000px")
 )
-
 
 # Server logic ----
 server <- function(input, output) {
@@ -95,27 +142,65 @@ server <- function(input, output) {
   })
   
   output$speciesPlot <- renderPlot({
-    yaxisSym <- sym(input$yaxis)
-    ColorSym <- sym(input$color)
-    FacetSym <- sym(input$facet)
     xaxisSym <- sym(input$xaxis)
+    yaxisSym <- sym(input$yaxis)
+    colorSym <- sym(input$color)
+    shapeSym <- sym(input$shape)
+    facetWrapSym <- sym(input$facetWrap)
+    facetXSym <- sym(input$facetX)
+    facetYSym <- sym(input$facetY)
+    shapeSym <- sym(input$shape)
+    #sizeSym <- sym(as.character(input$size))
     
-    plot <- ggplot(finalData()) +
-      {if(input$color != "None"){
-        geom_point(aes(x = !!xaxisSym, y = !!yaxisSym, color = !!ColorSym))
+    ggplot(finalData()) +
+      {
+        if (input$color != "None" & input$shape != "None") {
+          geom_point(aes(
+            x = !!xaxisSym,
+            y = !!yaxisSym,
+            color = !!colorSym,
+            shape = !!shapeSym
+          ),
+          size = input$size)
+        }
+        else if (input$color != "None") {
+          geom_point(aes(
+            x = !!xaxisSym,
+            y = !!yaxisSym,
+            color = !!colorSym
+          ))
+        }
+        else if (input$shape != "None") {
+          geom_point(aes(
+            x = !!xaxisSym,
+            y = !!yaxisSym,
+            shape = !!shapeSym
+          ),
+          size = input$size)
         }
         else{
-          geom_point(aes(x = !!xaxisSym, y = !!yaxisSym))
-        }}+
-      #geom_point(aes(x = Stress_h, y = !!yaxisym, color = !!ColorSym))+#, colour = as.factor(CultureId))) +
-      {if(input$facet != "None"){
-        facet_wrap(vars(!!FacetSym))
-      }}+
-      #facet_wrap(vars(Inhibitor))+
-      #facet_grid(vars(Sp), vars(Inhibitor))+
-      theme_bw()+
+          geom_point(aes(x = !!xaxisSym, y = !!yaxisSym),
+                     size = input$size)
+        }
+      } +
+      {
+        if (input$facetWrap != "None") {
+          facet_wrap(vars(!!facetWrapSym))
+        }
+        else {
+          if (input$facetX != "None" & input$facetY != "None") {
+            facet_grid(vars(!!facetXSym), vars(!!facetYSym))
+          }
+          else if (input$facetX != "None") {
+            facet_grid(rows = vars(!!facetXSym))
+          }
+          else if (input$facetY != "None") {
+            facet_grid(cols = vars(!!facetYSym))
+          }
+        }
+      } +
+      theme_bw() +
       ggtitle(paste("Protein Target:", input$target))
-    
   })
 }
 
